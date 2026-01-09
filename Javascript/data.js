25e60c2c25d3076592cf7eba3dce32d003c18007
safdaraliabbasi <safdaraliabbasi1345@gmail.com>
2026-01-09 10:01:07 +0000

    Sameer

--- Javascript/data.js
+++ Javascript/data.js
@@ -1,1 +1,7 @@
-console.log("This is my javascript page");
+// console.log("This is my javascript page");
+let myDate = new Date();
+
+console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
